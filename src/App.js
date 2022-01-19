d11277f383a7202ccfa04f695b5cc2bfe9d2787e
jessica <fuerte.jessica10@gmail.com>
2022-01-19 17:57:39 -0600

    forgot to commit oops

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
 import './App.css';
+import Suggestion from './components/Suggestion.js'
+import Favorites from './components/Favorites'
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Are you bored yet?</h1>
+      <div className="boxes">
+        <Suggestion/>
+        <Favorites/>
+      </div>
     </div>
   );
 }
